805fc8d906021a850307fec68a3fe8dba1d4de47
Ioan <ioan_solderea@yahoo.com>
2020-05-08 22:49:10 +0300

    make it better

---
+++ logic.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',() => {
+
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+ function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    console.log("I ca see"+items.length)
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+
+})
